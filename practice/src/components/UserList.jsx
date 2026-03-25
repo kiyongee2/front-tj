70b2252ea42898f8273c8b8b8262fe075cf4447b
kiyongee2 <kiyongee2@gmail.com>
2026-03-25 17:13:57 +0900

    map

--- practice/src/components/UserList.jsx
+++ practice/src/components/UserList.jsx
@@ -9,6 +9,13 @@ const UserList = () => {
   // 배열
   const carts = ["라면", "달걀", "콩나물"]
 
+  // 객체
+  const fruits = [
+    { id: Date.now(), name: "사과", price: 1000 },
+    { id: Date.now() + 1, name: "바나나", price: 2000 },
+    { id: Date.now() + 2, name: "포도", price: 3000 }
+  ]
+
   return (
     <div>
       <h1>User</h1>
@@ -19,9 +26,8 @@ const UserList = () => {
       </ul>
 
       <ul style={{ listStyleType: "none", padding: 0 }}>
-        {carts.map((cart, index) => (
-          // <li key={index}>{index + 1}. {cart}</li>
-          <li key={index}>{`${index + 1}. ${cart}`}</li>
+        {fruits.map(fruit => (
+          <li key={fruit.id}>{fruit.name} - {fruit.price}원</li>
         ))}
       </ul>
     </div>
